975562fd3f7c2b13cd4bd2aebabdbb5d267f51af
itsDevJokers <jonathankarongkong97@gmail.com>
2022-07-11 11:15:03 +0700

    update say hello with params name

--- go-testing.go
+++ go-testing.go
@@ -1,5 +1,5 @@
 package gomodtesting
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/itsDevJokers/go-mod-testing
+module github.com/itsDevJokers/go-mod-testing/v2
 
 go 1.18
